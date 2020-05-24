60970fac73332b7f59c3c74e9f0c619004a5365d
katherineES98 <sosa96aleman@gmail.com>
2020-05-23 23:39:24 -0600

    Refactorizar mapa

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,63 +1,57 @@
-const url = '../../Cat-by/backend/api/empresas.php';
-const url1='../../Cat-by/backend/api/loginEmpresa.php';
-const tests='../../Cat-by/backend/api/empresasFavoritas.php';
+const url = "../../Cat-by/backend/api/empresas.php";
+const url1 = "../../Cat-by/backend/api/loginEmpresa.php";
+const tests = "../../Cat-by/backend/api/empresasFavoritas.php";
 var empresas = [];
 
-console.log("imprimeindo windon location",window.location.search.substring(1))
+console.log("imprimeindo windon location", window.location.search.substring(1));
 
-if(window.location.search.substring(1)){
-  obtenerEmpresaId()
+if (window.location.search.substring(1)) {
+  obtenerEmpresaId();
   //updateForm()
 }
 //const url1="../api/productos.php";
 
-function updateFormValue(data){
-  document.getElementById('nombreEmpresa').value=data.nombreEmpresa
-  document.getElementById('correo').value=data.correo
-  document.getElementById('contrasena').value=data.contrasena
-  document.getElementById('mision').value=data.mision
-  document.getElementById('vision').value=data.vision
-  document.getElementById('descripcionEmpresa').value=data.descripcionEmpresa
-  document.getElementById('direccion').value=data.direccion
-  document.getElementById('pais').value=data.pais
-  document.getElementById('ciudad').value=data.ciudad
-  document.getElementById('telefono').value=data.telefono
-  document.getElementById('redesSociales').value=data.redesSociales
-  document.getElementById('URL').value=data.URL
-  document.getElementById('latitud').value=data.latitud
-  document.getElementById('longitud').value=data.longitud
-  document.getElementById('pago').value=data.pago
-  document.getElementById('plan').value=data.plan
-  document.getElementById('propietario').value=data.propietario
-  document.getElementById('numeroTarjeta').value=data.numeroTarjeta
-  document.getElementById('vencimiento').value=data.vencimiento
-  document.getElementById('cvv').value=data.cvv
- 
+function updateFormValue(data) {
+  document.getElementById("nombreEmpresa").value = data.nombreEmpresa;
+  document.getElementById("correo").value = data.correo;
+  document.getElementById("contrasena").value = data.contrasena;
+  document.getElementById("mision").value = data.mision;
+  document.getElementById("vision").value = data.vision;
+  document.getElementById("descripcionEmpresa").value = data.descripcionEmpresa;
+  document.getElementById("direccion").value = data.direccion;
+  document.getElementById("pais").value = data.pais;
+  document.getElementById("ciudad").value = data.ciudad;
+  document.getElementById("telefono").value = data.telefono;
+  document.getElementById("redesSociales").value = data.redesSociales;
+  document.getElementById("URL").value = data.URL;
+  document.getElementById("latitud").value = data.latitud;
+  document.getElementById("longitud").value = data.longitud;
+  document.getElementById("pago").value = data.pago;
+  document.getElementById("plan").value = data.plan;
+  document.getElementById("propietario").value = data.propietario;
+  document.getElementById("numeroTarjeta").value = data.numeroTarjeta;
+  document.getElementById("vencimiento").value = data.vencimiento;
+  document.getElementById("cvv").value = data.cvv;
 }
 var empresas = [];
 //var empresaSeleccionada;
 
-
 function obtenerEmpresas() {
-  
   axios({
     method: "GET",
     url: url,
-    responseType: "json"
+    responseType: "json",
   })
     .then((res) => {
       console.log(res.data);
       this.empresas = res.data;
       //al  momento que responda el servidor le vamos asignar el arreglo
       generarEmpresa();
-      
     })
     .catch((error) => {
       console.error(error);
     });
 }
-
-
 
 //generar las cards de una empresa
 function generarEmpresa() {
@@ -104,40 +98,37 @@ function generarEmpresa() {
 		`;
   }
 }
-function optenerId(){
-  let kokie 
-  let idEmpresa
+function optenerId() {
+  let kokie;
+  let idEmpresa;
   let aCookies = document.cookie.split(";");
   for (let i = 0; i < aCookies.length; i++) {
-    kokie=aCookies[i].split("=")
-    if(kokie[0]===" id"){
-      console.log(kokie[1])
-       idEmpresa=kokie[1]
+    kokie = aCookies[i].split("=");
+    if (kokie[0] === " id") {
+      console.log(kokie[1]);
+      idEmpresa = kokie[1];
     }
   }
-  return idEmpresa
+  return idEmpresa;
 }
-function obtenerEmpresaId(){
-  
-  console.log("id a buscar: ",optenerId());
+function obtenerEmpresaId() {
+  console.log("id a buscar: ", optenerId());
   axios({
     method: "GET",
-    url: url+`?id=${optenerId()}`,
-    responseType: "json"
+    url: url + `?id=${optenerId()}`,
+    responseType: "json",
   })
     .then((res) => {
       console.log(res.data);
       this.empresas = res.data;
-      updateFormValue(res.data)
+      updateFormValue(res.data);
       //al  momento que responda el servidor le vamos asignar el arreglo
-     // generarEmpresa();
-      
+      // generarEmpresa();
     })
     .catch((error) => {
       console.error(error);
     });
 }
-
 
 //cargalas sucursales de la modal
 function cargarSucursales(nombreS) {
@@ -191,12 +182,20 @@ function cargarSucursales(nombreS) {
 						<div class="col-sm-6">
 						<p>${empresas[i].sucursales[j].telefono}</p>
 						</div>
+            </div>
+            <div class="form-group row">
+						<label for="staticEmail" class="col-sm-4 col-form-label">Mapa</label>
+						<div class="col-sm-6">
+						<p><a href="mapa.html?log=${empresas[i].longitud}&lat=${
+          empresas[i].latitud
+        }">Ver Mapa</a></p>
+						</div>
 					  </div>
 					  </center>
 				  </form>
 			  
-			</div>
-		  </div>
+      </div>
+      </div>
 				
 
 
@@ -221,202 +220,158 @@ function llenarSelectSucursal(sucursalE) {
   return sucursales1;
 }
 //genera extrellas no se si funciona aun
-function valoracion(valor){
-	console.log(valor);
+function valoracion(valor) {
+  console.log(valor);
 }
 
-
-
-//funcion guardar una empresa  revisarlaaaa porque la data da null pero guarda ya 
-function guardarEmpresa(){
+//funcion guardar una empresa  revisarlaaaa porque la data da null pero guarda ya
+function guardarEmpresa() {
   let empresa = {
-    nombreEmpresa: document.getElementById('nombreEmpresa').value,
-    descripcionEmpresa: document.getElementById('descripcionEmpresa').value,
-    correo: document.getElementById('correo').value,
-    contrasena: document.getElementById('contrasena').value,
+    nombreEmpresa: document.getElementById("nombreEmpresa").value,
+    descripcionEmpresa: document.getElementById("descripcionEmpresa").value,
+    correo: document.getElementById("correo").value,
+    contrasena: document.getElementById("contrasena").value,
     logo: "img/empresas/" + document.getElementById("logo").files[0].name,
     banner: "img/empresas/" + document.getElementById("banner").files[0].name,
-    mision: document.getElementById('mision').value,
-    vision: document.getElementById('vision').value,
-    direccion: document.getElementById('direccion').value,
-    pais: document.getElementById('pais').value,
-    ciudad: document.getElementById('ciudad').value,
-    telefono: document.getElementById('telefono').value,
-    redesSociales: document.getElementById('redesSociales').value,
-    URL: document.getElementById('URL').value,
-    latitud: document.getElementById('latitud').value,
-    longitud: document.getElementById('longitud').value,
-    pago: document.getElementById('pago').value,
-    propietario: document.getElementById('propietario').value,
-    numeroTarjeta: document.getElementById('numeroTarjeta').value,
-    vencimiento: document.getElementById('vencimiento').value,
-    cvv: document.getElementById('cvv').value,
-    plan: document.getElementById('plan').value,
-    sucursales:[],
-    productos:[],
-    promociones:[]
+    mision: document.getElementById("mision").value,
+    vision: document.getElementById("vision").value,
+    direccion: document.getElementById("direccion").value,
+    pais: document.getElementById("pais").value,
+    ciudad: document.getElementById("ciudad").value,
+    telefono: document.getElementById("telefono").value,
+    redesSociales: document.getElementById("redesSociales").value,
+    URL: document.getElementById("URL").value,
+    latitud: document.getElementById("latitud").value,
+    longitud: document.getElementById("longitud").value,
+    pago: document.getElementById("pago").value,
+    propietario: document.getElementById("propietario").value,
+    numeroTarjeta: document.getElementById("numeroTarjeta").value,
+    vencimiento: document.getElementById("vencimiento").value,
+    cvv: document.getElementById("cvv").value,
+    plan: document.getElementById("plan").value,
+    sucursales: [],
+    productos: [],
+    promociones: [],
+  };
+  console.log("Empresa a guardar", empresa);
 
-};
-console.log('Empresa a guardar', empresa)
-
-axios({
-  method:'POST',
-  url:url,
-  responseType:'json',
-  data:empresa
- }).then(res=>{
-     console.log(res);
-     
- }).catch(error=>{
-     console.error(error);
- });
-
- 
-
-
+  axios({
+    method: "POST",
+    url: url,
+    responseType: "json",
+    data: empresa,
+  })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
 
 //funcion para actualizar empresa aun no lo termino revisar depues
 
-function actualizarEmpresa(){
+function actualizarEmpresa() {
   let empresa = {
-    nombreEmpresa: document.getElementById('nombreEmpresa').value,
-    descripcionEmpresa: document.getElementById('descripcionEmpresa').value,
-    correo: document.getElementById('correo').value,
-    contrasena: document.getElementById('contrasena').value,
-    logo: "Cat-by/frontend/img/" + document.getElementById("logo").files[0].name,
-    banner: "Cat-by/frontend/img/" + document.getElementById("banner").files[0].name,
-    mision: document.getElementById('mision').value,
-    vision: document.getElementById('vision').value,
-    direccion: document.getElementById('direccion').value,
-    pais: document.getElementById('pais').value,
-    ciudad: document.getElementById('ciudad').value,
-    telefono: document.getElementById('telefono').value,
-    redesSociales: document.getElementById('redesSociales').value,
-    URL: document.getElementById('URL').value,
-    latitud: document.getElementById('latitud').value,
-    longitud: document.getElementById('longitud').value,
-    pago: document.getElementById('pago').value,
-    propietario: document.getElementById('propietario').value,
-    numeroTarjeta: document.getElementById('numeroTarjeta').value,
-    vencimiento: document.getElementById('vencimiento').value,
-    cvv: document.getElementById('cvv').value,
-    plan: document.getElementById('plan').value
-  
-
-};
-console.log('Empresa a actualizar', empresa );
-//servidor ahora
-axios({
-  method:'PUT',
-  url:url +`?id=${optenerId()}`,
-  responseType:'json',
-  data:empresa
- }).then(res=>{
-     console.log(res);
- }).catch(error=>{
-     console.error(error);
- });
-
- 
-
- 
-
-
-}
-
-
-
-function loginEmpresa(){
+    nombreEmpresa: document.getElementById("nombreEmpresa").value,
+    descripcionEmpresa: document.getElementById("descripcionEmpresa").value,
+    correo: document.getElementById("correo").value,
+    contrasena: document.getElementById("contrasena").value,
+    logo:
+      "Cat-by/frontend/img/" + document.getElementById("logo").files[0].name,
+    banner:
+      "Cat-by/frontend/img/" + document.getElementById("banner").files[0].name,
+    mision: document.getElementById("mision").value,
+    vision: document.getElementById("vision").value,
+    direccion: document.getElementById("direccion").value,
+    pais: document.getElementById("pais").value,
+    ciudad: document.getElementById("ciudad").value,
+    telefono: document.getElementById("telefono").value,
+    redesSociales: document.getElementById("redesSociales").value,
+    URL: document.getElementById("URL").value,
+    latitud: document.getElementById("latitud").value,
+    longitud: document.getElementById("longitud").value,
+    pago: document.getElementById("pago").value,
+    propietario: document.getElementById("propietario").value,
+    numeroTarjeta: document.getElementById("numeroTarjeta").value,
+    vencimiento: document.getElementById("vencimiento").value,
+    cvv: document.getElementById("cvv").value,
+    plan: document.getElementById("plan").value,
+  };
+  console.log("Empresa a actualizar", empresa);
+  //servidor ahora
   axios({
-    method:'POST',
-    url:url1,
-    responseType:'json',
-    data:{
-      correo:document.getElementById("correoEmpresa").value,
-      contrasena:document.getElementById("passwordEmpresa").value
-
-    }
-   }).then(res=>{
-    if (res.data.codigoResultado==1) {
-      window.location.href="./cuentaEmpresarial.php";
-      
-  } else {
-      document.getElementById('error').style.display='block';
-      document.getElementById('error').innerHTML=res.data.mensaje;
-      
-  }
-       console.log(res);
-       
-   }).catch(error=>{
-       console.error(error);
-   });
-  
-
-
-
-
+    method: "PUT",
+    url: url + `?id=${optenerId()}`,
+    responseType: "json",
+    data: empresa,
+  })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
 
-function obtenerIdUsuario(){
-  let kokie 
-  let idUsuario
+function loginEmpresa() {
+  axios({
+    method: "POST",
+    url: url1,
+    responseType: "json",
+    data: {
+      correo: document.getElementById("correoEmpresa").value,
+      contrasena: document.getElementById("passwordEmpresa").value,
+    },
+  })
+    .then((res) => {
+      if (res.data.codigoResultado == 1) {
+        window.location.href = "./cuentaEmpresarial.php";
+      } else {
+        document.getElementById("error").style.display = "block";
+        document.getElementById("error").innerHTML = res.data.mensaje;
+      }
+      console.log(res);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+function obtenerIdUsuario() {
+  let kokie;
+  let idUsuario;
   let aCookies = document.cookie.split(";");
   for (let i = 0; i < aCookies.length; i++) {
-    kokie=aCookies[i].split("=")
-    if(kokie[0]===" id"){
-      console.log(kokie[1])
-       idUsuario=kokie[1]
+    kokie = aCookies[i].split("=");
+    if (kokie[0] === " id") {
+      console.log(kokie[1]);
+      idUsuario = kokie[1];
     }
   }
-  return idUsuario
+  return idUsuario;
 }
 
+function empresaFav(k) {
+  console.log("este es la empresa favorita al guardar", k);
+  let favoriteCompany = {
+    logo: empresas[k].logo,
+    nombreEmpresa: empresas[k].nombreEmpresa,
+    descripcionEmpresa: empresas[k].descripcionEmpresa,
+    direccion: empresas[k].direccion,
+  };
+  console.log("Empresa Favorita a guardar", favoriteCompany);
 
-function empresaFav(k){
-console.log("este es la empresa favorita al guardar",k);
-let favoriteCompany = {
-  logo:empresas[k].logo,
-  nombreEmpresa:empresas[k].nombreEmpresa,
-  descripcionEmpresa:empresas[k].descripcionEmpresa,
-  direccion:empresas[k].direccion 
-
-
-};
-console.log('Empresa Favorita a guardar', favoriteCompany)
-
-axios({
-  method:'POST',
-  url:tests +`?id=${obtenerIdUsuario()}`,
-  responseType:'json',
-  data:favoriteCompany
- }).then(res=>{
-     console.log(res);
-     
- }).catch(error=>{
-     console.error(error);
- });
-
-
-
-
-  
+  axios({
+    method: "POST",
+    url: tests + `?id=${obtenerIdUsuario()}`,
+    responseType: "json",
+    data: favoriteCompany,
+  })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
