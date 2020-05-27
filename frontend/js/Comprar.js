de75e88dea0a504c69bfaf2173edaf9910071dca
Katherine <katsantos1997@gmail.com>
2020-05-27 13:16:22 -0600

    obtener productos de carrito en compras

--- frontend/js/Comprar.js
+++ frontend/js/Comprar.js
@@ -1,21 +1,67 @@
 
 
-function login(){
-  
-    validarCampoVacio('nombrepro');
-    validarCampoVacio('numerot');
-    validarCampoVacio('vencimiento');
-    validarCampoVacio('cvv');
-   
-   
-}
 
-function validarCampoVacio(id){
-    if (document.getElementById(id).value == ''){
-        document.getElementById(id).classList.remove('input-success');
-        document.getElementById(id).classList.add('input-error');
-    }else{ 
-        document.getElementById(id).classList.remove('input-error');
-        document.getElementById(id).classList.add('input-success');
+const url='../../Cat-by/backend/api/usuarios.php';
+const test='../../Cat-by/backend/api/carrito.php';
+var usuarios = [];
+
+
+function obtenerIdUsuario(){
+    let kokie 
+    let idUsuario
+    let aCookies = document.cookie.split(";");
+    for (let i = 0; i < aCookies.length; i++) {
+      kokie=aCookies[i].split("=")
+      if(kokie[0]===" id"){
+        console.log(kokie[1])
+         idUsuario=kokie[1]
+      }
     }
-}
+    return idUsuario
+  }
+
+    
+    
+
+function optenerMisProductosCarrito() {
+    console.log("id a buscar: ",obtenerIdUsuario());
+    axios({
+      method: "GET",
+      url: test+`?id=${obtenerIdUsuario()}`,
+      responseType: "json"
+    })
+      .then((res) => {
+        console.log(res.data);
+        this.usuarios = res.data;
+        llenarCarritoCompra(res.data)
+       
+        //al  momento que responda el servidor le vamos asignar el arreglo
+       // generarEmpresa();
+        
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  
+    }
+
+ function llenarCarritoCompra(data){
+ console.log(data);
+ document.getElementById("compra-producto").innerHTML="";
+ for (let i = 0; i < data.carrito.length; i++) {
+     document.getElementById("compra-producto").innerHTML+=`
+     <div class="d-flex">
+     <h6>${data.carrito[i].nombreProducto}</h6>
+     <div class="ml-auto text3"> ${data.carrito[i].precioAhora} </div>
+</div>
+
+     
+     `
+    
+     
+ }
+
+
+
+
+ } 
