d75898e4b217948f0d329f2c20cc8281fc48aef8
Katherine <katsantos1997@gmail.com>
2020-05-27 02:12:34 -0600

    visualizar logo y banner de empresa

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -375,3 +375,40 @@ function empresaFav(k) {
       console.error(error);
     });
 }
+
+
+//funcion para visulaizar las imagen en el div del input Logo:
+function readURLogo(input) {
+  if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function(e) {
+          $('#img-visualizar-logo').attr('src', e.target.result);
+      }
+
+
+      reader.readAsDataURL(input.files[0]);
+  }
+}
+
+   //('#image').change(function() {
+  //readURL(this);
+//});    
+
+//funcion para visulaizar las imagen en el div del input Banner:
+function readURL(input) {
+  if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function(e) {
+          $('#img-visualizar-banner').attr('src', e.target.result);
+      }
+
+
+      reader.readAsDataURL(input.files[0]);
+  }
+}
+
+   //('#image').change(function() {
+  //readURL(this);
+//});
